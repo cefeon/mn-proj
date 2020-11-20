e70d6228592db98bdd5b719e198ed5d92dcacfcc
cefeon <cefeon@outlook.com>
2020-11-20 02:23:07 +0100

    fixed plot_g

--- plot_g.m
+++ plot_g.m
@@ -1,19 +1,13 @@
 function plot_g(R1,R2,C1,C2,fmin,fmax)
-    %NOT WORKING AT ALL, TODO%
-    R1 = 30;
-    R2 = 10;
-    C1 = 0.1e-6;
-    C2 = 0.2e-6;
-    fmin = 1;
-    fmax = 10000;
-    F = @(x) 20*log10(abs((x/(C2*R2)) / (x^2+x*(1/(C1*R1)+1/(C1*R2)+1/(C2*R2)+1/(C1*C2*R1*R2)))));
-    x = [fmin : 0.1 : fmax];
+    F = @(x) 20*log10 (abs((x/(C2*R2)) / (x^2+x*(1/(C1*R1)+1/(C1*R2)+1/(C2*R2))+1/(C1*C2*R1*R2))));
+    x = logspace (log10(fmin),log10(fmax));
+    z = 2i*pi*x;
+    
+    y = zeros(length(x));
     for i = 1 : length(x)
-        x(i) = j*2*pi*x(i);
+        y(i) = F(z(i));
     end
-    for i = 1 : length(x)
-        y(i) = F(x(i));
-    end
-    semilogx(y);
+    
+    semilogx(x,y);
     grid on
 end
