bf4f4cb5f71ea67b6829587defbd3c67c228924a
cefeon <cefeon@outlook.com>
2020-11-20 03:39:53 +0100

    Changed variable names in bandpass.m and calka.m

--- calka.m
+++ calka.m
@@ -8,19 +8,20 @@ function calka()
     freq = 100000;
     t = [ 0 : h : 1 ]; 
     y = [0 0]';
-    e = @(t) sin(2*pi*t*freq);
     
+    e = @(t) sin(2*pi*t*freq);
     dy = @(t,y) ...
         [  1/C1 * ( (e(t) - y(1) - y(2))/R2 + (e(t) - y(1))/R1 )
            1/C2 * ( (e(t) - y(1) - y(2))/R2 + y(2)/RL ) ];
-    results = euler(t, h, dy);
-    
-    u1 = results(1,:);
-    u2 = results(2,:);
+    u = euler(t, h, dy);
+
+    u1 = u(1,:);
+    u2 = u(2,:);
     
     for i=1 : length(t)
         dP(i) = (e(t(i))-u1(i))^2/R1 + (e(t(i))-u1(i)-u2(i))^2/R1;
     end
+    
     
     %metoda prostokatów
     prostokat = dP(1:end-1) * h;
