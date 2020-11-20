d7410b241e862221fde447d51d93c8f9fc4e0e88
cefeon <cefeon@outlook.com>
2020-11-20 05:39:52 +0100

    Added beuler

--- calka.m
+++ calka.m
@@ -12,7 +12,7 @@ function calka()
     dy = @(t,y) ...
         [  1/C1 * ( (e(t) - y(1) - y(2))/R2 + (e(t) - y(1))/R1 )
            1/C2 * ( (e(t) - y(1) - y(2))/R2 - y(2)/RL ) ];
-    u = euler(t, h, dy);
+    u = beuler(t, h, dy);
     
     %obliczanie wartości P ze wzoru
     for i=1 : length(t)
@@ -23,13 +23,16 @@ function calka()
     
     %złożona metoda prostokątów lewych
     prostokat = dP(1:end-1) * h;
-    prostokaty = sum(prostokat)
+    prostokaty = sum(prostokat);
     
     %złożona metoda parabol (Simpsona)
     for i = 1 : 2 : length(t)-2
         simpson((i + 1) / 2) = h/3*(dP(i)+4*dP(i+1)+dP(i+2));
     end
-    parabole = sum(simpson)
+    parabole = sum(simpson);
+    
+    fprintf('Metoda prostokatów: %e \n\n',prostokaty);
+    fprintf('Metoda Simpsona: %e \n\n',parabole);
 end
 
 
@@ -38,5 +41,13 @@ function y = euler(t,h,f)
     y = [0 0]';
     for i = 1 : length(t)-1
        y(:, i+1) = y(:, i) + h * f(t(i), y(:, i));
+    end
+end
+
+function y = beuler(t,h,f)
+    y = [0 0]';
+    for i = 1 : length(t)-1
+        prediction = y(:, i) + h/2 * f(t(i), y(:, i));
+        y(:, i+1) = y(:, i) + h * f(t(i) +h/2, prediction);
     end
 end
